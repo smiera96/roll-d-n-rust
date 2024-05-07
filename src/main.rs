154ff41d534760ba2b3e24212c9eefd443c01ed2
smiera96 <s.miera96@gmail.com>
2024-05-07 21:18:02 +0200

    ENV added to docker

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod apps;
 mod core;
 
-use std::io;
+use std::{env, io};
 use actix_web::{App, HttpServer, middleware};
+use dotenv::dotenv;
 use apps::roll_d_n_rust_api::routes as roll_d_n_rust_api_routes;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    dotenv().ok();
+
     let host = find_env("HOST");
     let port = find_env("PORT");
     let url  = format!("{}:{}", host, port);
@@ -26,7 +29,7 @@ async fn main() -> io::Result<()> {
 }
 
 fn find_env(key: &str) -> String {
-    match dotenv::var(key) {
+    match env::var(key) {
         Ok(v) => v,
         Err(e) => panic!("${} is not set ({})", key, e)
     }
